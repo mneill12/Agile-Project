659280a6b2133f6f8df1af4f09f7d66de88e73bd
Ryan Meharg <rmeharg01@qub.ac.uk>
2015-10-27 10:42:39 +0000

    Adding account service contract to client side

---
+++ CSC3045.Agile.Client.Contracts/Service Contracts/IAccountService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC3045.Agile.Client.Contracts.Service_Contracts
+{
+    class IAccountService
+    {
+    }
+}
