5e4625a85773dd6839d839eddbf73a9db5e2c2d6
Ryan Meharg <rmeharg01@qub.ac.uk>
2015-10-27 23:36:25 +0000

    Adding service factory for DI and account client

---
+++ CSC3045.Agile.Client.Proxies/AccountClient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using CSC3045.Agile.Client.Contracts;
+using CSC3045.Agile.Client.Entities;
+using Core.Common.ServiceModel;
+
+namespace CSC3045.Agile.Client.Proxies
+{
+    [Export(typeof(IAccountService))]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class AccountClient : ClientBase<IAccountService>, IAccountService
+    {
+        public Account GetAccountInfo(string loginEmail)
+        {
+            return Channel.GetAccountInfo(loginEmail);
+        }
+
+        public void UpdateAccountInfo(Account account)
+        {
+            Channel.UpdateAccountInfo(account);
+        }
+    }
+}
