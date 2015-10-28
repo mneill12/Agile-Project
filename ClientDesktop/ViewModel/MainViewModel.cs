020e259c6805b45d989f1e813e0100ecc57cfbda
Ryan Meharg <rmeharg01@qub.ac.uk>
2015-10-28 03:11:46 +0000

    Adding packages to ClientDesktop

---
+++ ClientDesktop/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.UI.Core;
+
+namespace CSC3045.Agile.Presentation.ClientDesktop.ViewModel
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    class MainViewModel : ViewModelBase
+    {
+        
+    }
+}
