0264b9be8fb4a9127a34ce4d2667582acc328065
Joe Flynn <Joe Flynn>
2015-11-01 23:41:13 +0000

    - Added eager loading Account entity retrieval test to console app. - Updated entities to remove unesseccary members;

--- CSC3045.Agile.ServiceHost.Console/Program.cs
+++ CSC3045.Agile.ServiceHost.Console/Program.cs
@@ -6,6 +6,7 @@ using CSC3045.Agile.Business.Entities;
 using CSC3045.Agile.Business.Services;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.Principal;
 using CSC3045.Agile.Data;
 
 namespace CSC3045.Agile.ServiceHost.Console
@@ -107,6 +108,7 @@ namespace CSC3045.Agile.ServiceHost.Console
             {
 
                 ICollection<UserStory> userStorySet = GetUserStories();
+                ICollection<Account> accountSet = GetAccounts();
 
                 foreach (UserStory userStoryTest in userStorySet)
                 {
@@ -115,21 +117,21 @@ namespace CSC3045.Agile.ServiceHost.Console
                         "Testing populated UserStory returns associated tasks and acceptance criteria with one DB call");
                     System.Console.WriteLine(
                         "=============================================================================================");
-                    System.Console.WriteLine("Story Number: " + userStoryTest.StoryNumber);
-                    System.Console.WriteLine("Description: " + userStoryTest.Description);
-                    System.Console.WriteLine("Story Points: " + userStoryTest.StoryPoints);
-                    System.Console.WriteLine("Status: " + userStoryTest.Status.StoryStatusName);
+                    System.Console.WriteLine("Story Number:\t" + userStoryTest.StoryNumber);
+                    System.Console.WriteLine("Description:'t" + userStoryTest.Description);
+                    System.Console.WriteLine("Story Points:\t" + userStoryTest.StoryPoints);
+                    System.Console.WriteLine("Status:\t" + userStoryTest.Status.StoryStatusName);
                     System.Console.WriteLine();
-                    System.Console.WriteLine("\t Tasks: ");
+                    System.Console.WriteLine("\t Tasks:");
                     System.Console.WriteLine();
 
-                    System.Console.WriteLine("\t ===============================================");
+                    System.Console.WriteLine("\t===============================================");
                     foreach (StoryTask tsk in userStoryTest.AssociatedTasks)
                     {
-                        System.Console.WriteLine("\t Title: " + tsk.Title);
-                        System.Console.WriteLine("\t Description: " + tsk.Description);
-                        System.Console.WriteLine("\t Hours: " + tsk.Hours);
-                        System.Console.WriteLine("\t Blocked Status: " + tsk.IsBlocked);
+                        System.Console.WriteLine("\tTitle:\t" + tsk.Title);
+                        System.Console.WriteLine("\tDescription:\t" + tsk.Description);
+                        System.Console.WriteLine("\tHours:\t" + tsk.Hours);
+                        System.Console.WriteLine("\tBlocked Status:\t" + tsk.IsBlocked);
                         // System.Console.WriteLine("\t Status: " + tsk.CurrentStatus.StoryStatusName);
                         System.Console.WriteLine("\t ===============================================");
                     }
@@ -140,19 +142,49 @@ namespace CSC3045.Agile.ServiceHost.Console
 
                     foreach (AcceptanceCriteria ac in userStoryTest.AcceptanceCriteria)
                     {
-                        System.Console.WriteLine("\t ===============================================");
-                        System.Console.WriteLine("\t Scenario: " + ac.Scenario);
+                        System.Console.WriteLine();
+                        System.Console.WriteLine("\t Scenario:\t" + ac.Scenario);
 
-                        System.Console.WriteLine("\t ===============================================");
+                        System.Console.WriteLine();
                         foreach (Criteria cri in ac.Criteria)
                         {
                             System.Console.WriteLine("\t " + cri.CriteriaType);
                             System.Console.WriteLine("\t " + cri.CriteriaOutline);
-                            System.Console.WriteLine("\t ===============================================");
+                            System.Console.WriteLine();
                         }
 
-                        System.Console.WriteLine("\t Satisfied?: " + ac.IsSatisfied);
+                        System.Console.WriteLine("\t Satisfied?:\t" + ac.IsSatisfied);
                     }
+                }
+
+                System.Console.WriteLine();
+                System.Console.WriteLine(
+                        "=============================================================================================");
+                System.Console.WriteLine(
+                        "Testing retrieving Account entities also retrieves associated User Roles for each account");
+                System.Console.WriteLine(
+                        "=============================================================================================");
+                System.Console.WriteLine();
+
+                foreach (Account account in accountSet)
+                {
+                    System.Console.WriteLine("Full Name: \t\t\t" + account.FirstName + " " + account.LastName);
+                    System.Console.WriteLine("Login Email:\t\t\t" + account.LoginEmail);
+                    System.Console.WriteLine("Password:\t\t\t" + account.Password);
+                    System.Console.WriteLine();
+                    System.Console.WriteLine("\tUser Roles:");
+                    System.Console.WriteLine();
+                    foreach (UserRole role in account.UserRoles)
+                    {
+
+                        System.Console.WriteLine("\tRole:\t\t\t\t" + role.UserRoleName);
+                        System.Console.WriteLine("\tPermission Level:\t\t" + role.PermissionLevel);
+                        System.Console.WriteLine();
+                    }
+
+                    System.Console.WriteLine();
+
+                    
                 }
 
             }
@@ -169,6 +201,18 @@ namespace CSC3045.Agile.ServiceHost.Console
                     .Include(s => s.Status)
                     .Include(s => s.AssociatedTasks.Select(p => p.CurrentStatus))
                     .Include(s => s.AcceptanceCriteria.Select(p => p.Criteria)).ToList();
+            }
+        }
+
+        // Eager loading query to load associated entities when retrieving Accounts
+        // @todo : move to account repo if/when CF DB works on all machines
+        static ICollection<Account> GetAccounts()
+        {
+            using (var db = new Csc3045AgileContext())
+            {
+                return db.AccountSet
+                    .Include(a => a.UserRoles)
+                    .ToList();
             }
         } 
 
