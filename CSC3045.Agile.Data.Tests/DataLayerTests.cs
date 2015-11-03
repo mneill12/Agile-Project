067a6660657e5082a48af17db61ae771fefe1fd2
Joe Flynn <Joe Flynn>
2015-11-03 13:10:25 +0000

    - Fixing unit tests. All teh greenz. Huzzah!

--- CSC3045.Agile.Data.Tests/DataLayerTests.cs
+++ CSC3045.Agile.Data.Tests/DataLayerTests.cs
@@ -103,7 +103,7 @@ namespace CSC3045.Agile.Data.Tests
 
         public IEnumerable<Account> GetAccounts()
         {
-            IEnumerable<Account> accounts = _AccountRepository.GetAccounts();
+            IEnumerable<Account> accounts = _AccountRepository.Get();
 
             return accounts;
         } 
@@ -130,7 +130,7 @@ namespace CSC3045.Agile.Data.Tests
             IAccountRepository accountRepository = 
                 _DataRepositoryFactory.GetDataRepository<IAccountRepository>();
 
-            IEnumerable<Account> accounts = accountRepository.GetAccounts();
+            IEnumerable<Account> accounts = accountRepository.Get();
 
             return accounts;
         } 
