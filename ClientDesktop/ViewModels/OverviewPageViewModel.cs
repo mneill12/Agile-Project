45f02b0a2a3bc472221691b78ae9a5713d3ed802
Matthew Neill <mneill12@qub.ac.uk>
2015-11-06 12:26:40 +0000

    Add Gary's MainView to up to date project, add in view model

---
+++ ClientDesktop/ViewModels/OverviewPageViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using Core.Common.UI.Core;
+
+namespace ClientDesktop.ViewModels
+{
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class OverviewPageViewModel : ViewModelBase
+    {
+        public OverviewPageViewModel()
+        {
+
+        }
+    }
+}
