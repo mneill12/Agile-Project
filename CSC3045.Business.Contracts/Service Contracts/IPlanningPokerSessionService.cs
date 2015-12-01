6f2025d7b65a043c672801c43e3cf9dc451a837c
40079166 <zeadie01@qub.ac.uk>
2015-12-01 21:23:27 +0000

    Adding Selection Status and Voting Status enums and Planning Poker Session Service

---
+++ CSC3045.Business.Contracts/Service Contracts/IPlanningPokerSessionService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using Core.Common.Exceptions;
+using Core.Common.Utils;
+using CSC3045.Agile.Business.Entities;
+
+namespace CSC3045.Agile.Business.Contracts
+{
+    [ServiceContract]
+    public interface IPlanningPokerSessionService
+    {
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void CreatePlanningPokerSession(PlanningPokerSession planningPokerSession);
+
+        [OperationContract]
+        PlanningPokerSession StartPlanningPokerSession(int planningPokerSessionId);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void AssignPointsToStory(int pointValue, string userStoryId);
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void ResetStoryStatus();
+
+        [OperationContract]
+        void SelectCurrentStory(int userStoryId);
+
+        [OperationContract]
+        void EndSession();
+
+        [OperationContract]
+        ICollection<ChatMessage> GetChatMessages();
+
+        [OperationContract]
+        PlanningPokerSelectionStatus GetSelectionStates();
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void SendMessage();
+
+        [OperationContract]
+        [TransactionFlow(TransactionFlowOption.Allowed)]
+        void SendVote();
+
+
+
+
+
+    }
+}
