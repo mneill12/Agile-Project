3ce3c8fb9e84618e9fbad72a2f59a6489d08c12e
Ryan Meharg <rmeharg01@qub.ac.uk>
2015-12-01 19:57:17 +0000

    Adding Chat Messages

--- CSC3045.Agile.Client.Entities/ChatMessage.cs
+++ CSC3045.Agile.Client.Entities/ChatMessage.cs
@@ -1,12 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Core.Common.Core;
 
 namespace CSC3045.Agile.Client.Entities
 {
-    class ChatMessage
+    public class ChatMessage : ObjectBase
     {
+        private int _MessageId;
+        private DateTime _Time;
+        private Account _Sender;
+        private string _Message;
+
+        public int MessageId
+        {
+            get { return _MessageId; }
+            set
+            {
+                if (_MessageId != value)
+                {
+                    _MessageId = value;
+                    OnPropertyChanged(() => MessageId);
+                }
+            }
+        }
+
+        public DateTime Time
+        {
+            get { return _Time; }
+            set
+            {
+                if (_Time != value)
+                {
+                    _Time = value;
+                    OnPropertyChanged(() => Time);
+                }
+            }
+        }
+
+        public Account Sender
+        {
+            get { return _Sender; }
+            set
+            {
+                if (_Sender != value)
+                {
+                    _Sender = value;
+                    OnPropertyChanged(() => Sender);
+                }
+            }
+        }
+
+        public string Message
+        {
+            get { return _Message; }
+            set
+            {
+                if (_Message != value)
+                {
+                    _Message = value;
+                    OnPropertyChanged(() => Message);
+                }
+            }
+        }
     }
-}
+}g
