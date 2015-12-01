3c5f2952df42be85c5d62922077058829a53113d
40079166 <zeadie01@qub.ac.uk>
2015-12-01 22:23:35 +0000

    Adding the service framework for planning poker

---
+++ CSC3045.Agile.Business.Services/PlanningPokerSessionService.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Security.Authentication;
+using System.ServiceModel;
+using Core.Common.Contracts;
+using Core.Common.Exceptions;
+using Core.Common.Utils;
+using CSC3045.Agile.Business.Common;
+using CSC3045.Agile.Business.Contracts;
+using CSC3045.Agile.Business.Entities;
+using CSC3045.Agile.Data.Contracts.Repository_Interfaces;
+
+namespace CSC3045.Agile.Business.Services
+{
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerCall,
+        ConcurrencyMode = ConcurrencyMode.Multiple,
+        ReleaseServiceInstanceOnTransactionComplete = false)]
+    public class PlanningPokerSessionService : ServiceBase, IPlanningPokerSessionService
+    {
+        [Import]
+        private IBusinessEngineFactory _BusinessEngineFactory;
+
+        [Import]
+        private IDataRepositoryFactory _DataRepositoryFactory;
+
+        public PlanningPokerSessionService()
+        {
+        }
+
+        /// <summary>
+        /// Used for testing, this service has the data repository initialized through dependency injection
+        /// </summary>
+        /// <param name="dataRepositoryFactory"></param>
+        public PlanningPokerSessionService(IDataRepositoryFactory dataRepositoryFactory)
+        {
+            _DataRepositoryFactory = dataRepositoryFactory;
+        }
+
+        /// <summary>
+        /// Used for testing, this service has the business engine initialized through dependency injection
+        /// </summary>
+        /// <param name="businessEngineFactory"></param>
+        public PlanningPokerSessionService(IBusinessEngineFactory businessEngineFactory)
+        {
+            _BusinessEngineFactory = businessEngineFactory;
+        }
+
+        /// <summary>
+        /// Used for testing, this service has the data repository & business engine initialized through dependency injection
+        /// </summary>
+        /// <param name="dataRepositoryFactory"></param>
+        /// <param name="businessEngineFactory"></param>
+        public PlanningPokerSessionService(IDataRepositoryFactory dataRepositoryFactory, IBusinessEngineFactory businessEngineFactory)
+        {
+            _DataRepositoryFactory = dataRepositoryFactory;
+            _BusinessEngineFactory = businessEngineFactory;
+        }
+
+        public void CreatePlanningPokerSession(PlanningPokerSession planningPokerSession)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public PlanningPokerSession StartPlanningPokerSession(int planningPokerSessionId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void AssignPointsToStory(int pointValue, string userStoryId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ResetStoryStatus()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SelectCurrentStory(int userStoryId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void EndSession()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<ChatMessage> GetChatMessages()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ICollection<Dictionary<int, PlanningPokerSelectionStatus>> GetSelectionStatuses()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendMessage(ChatMessage chatMessage)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendVote(int accountId)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CSC3045.Agile.Client.Proxies/PlanningPokerSessionClient.cs
@@ -0,0 +1,65 @@
+﻿using CSC3045.Agile.Client.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Common.Utils;
+using CSC3045.Agile.Client.Contracts;
+
+namespace CSC3045.Agile.Client.Proxies
+{
+    class PlanningPokerSessionClient : ClientBase<IPlanningPokerSessionService>, IPlanningPokerSessionService
+    {
+        public void CreatePlanningPokerSession(PlanningPokerSession planningPokerSession)
+        {
+            Channel.CreatePlanningPokerSession(planningPokerSession);
+        }
+
+        public PlanningPokerSession StartPlanningPokerSession(int planningPokerSessionId)
+        {
+            return Channel.StartPlanningPokerSession(planningPokerSessionId);
+        }
+
+        public void AssignPointsToStory(int pointValue, string userStoryId)
+        {
+            Channel.AssignPointsToStory(pointValue,userStoryId);
+        }
+
+        public void ResetStoryStatus()
+        {
+            Channel.ResetStoryStatus();
+        }
+
+        public void SelectCurrentStory(int userStoryId)
+        {
+            Channel.SelectCurrentStory(userStoryId);
+        }
+
+        public void EndSession()
+        {
+            Channel.EndSession();
+        }
+
+        public ICollection<ChatMessage> GetChatMessages()
+        {
+            return Channel.GetChatMessages();
+        }
+
+        public ICollection<Dictionary<int, PlanningPokerSelectionStatus>> GetSelectionStatuses()
+        {
+            return Channel.GetSelectionStatuses();
+        }
+
+        public void SendMessage(ChatMessage chatMessage)
+        {
+            Channel.SendMessage(chatMessage);
+        }
+
+        public void SendVote(int accountId, int pointNumber)
+        {
+            Channel.SendVote(accountId, pointNumber);
+        }
+    }
+}
