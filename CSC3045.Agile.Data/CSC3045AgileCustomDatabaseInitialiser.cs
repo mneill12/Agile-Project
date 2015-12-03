9d18008afb4d2219b08d82d3d044e39e35a65f0c
Matthew Neill <mneill12@qub.ac.uk>
2015-12-03 21:42:21 +0000

    remove sprint for initaliser

--- CSC3045.Agile.Data/CSC3045AgileCustomDatabaseInitialiser.cs
+++ CSC3045.Agile.Data/CSC3045AgileCustomDatabaseInitialiser.cs
@@ -6,7 +6,7 @@ using CSC3045.Agile.Business.Entities;
 
 namespace CSC3045.Agile.Data
 {
-    internal class Csc3045AgileCustomDatabaseInitialiser : DropCreateDatabaseAlways<Csc3045AgileContext>
+    internal class Csc3045AgileCustomDatabaseInitialiser : DropCreateDatabaseIfModelChanges<Csc3045AgileContext>
     {
         protected override void Seed(Csc3045AgileContext context)
         {
@@ -226,28 +226,6 @@ namespace CSC3045.Agile.Data
                 AllUsers = new List<Account>() { },
                 BacklogStories = new List<UserStory>() { }
             });
-
-            IList<Sprint> defaultSprints = new List<Sprint>();
-            defaultSprints.Add( new Sprint
-            {
-                SprintName    = "Mobile App Sprint 1",
-                StartDate = new DateTime(2015, 11, 1),
-                EndDate = new DateTime(2015, 11, 7)
-
-            });
-
-            defaultSprints.Add( new Sprint
-            {
-                SprintName    = "Mobile App Sprint 2",
-                StartDate = new DateTime(2015, 11, 10),
-                EndDate =   new DateTime(2015, 11, 17)
-
-            });
-
-            foreach (var sprint in defaultSprints)
-            {
-                context.SprintSet.Add(sprint);
-            }
             
             foreach (var project in defaultProjects)
             {
