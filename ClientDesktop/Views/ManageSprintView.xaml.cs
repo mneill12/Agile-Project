5c10b905417037e0d982c4f6ef52272cb0561ba1
Matthew Neill <mneill12@qub.ac.uk>
2015-12-03 23:34:42 +0000

    created Sprint backlog manage ui

---
+++ ClientDesktop/Views/ManageSprintView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClientDesktop.Views
+{
+    /// <summary>
+    /// Interaction logic for ManageSprintView.xaml
+    /// </summary>
+    public partial class ManageSprintView : Page
+    {
+        public ManageSprintView()
+        {
+            InitializeComponent();
+        }
+    }
+}
