4ebd5f040c3a45332d4664b77697ecd9ab6d5db3
Matthew Neill <mneill12@qub.ac.uk>
2015-12-03 23:59:21 +0000

    setup view and view model

--- ClientDesktop/ViewModels/ManageSprintViewModel.cs
+++ ClientDesktop/ViewModels/ManageSprintViewModel.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Windows;
+using System.ComponentModel;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Threading;
+using System.Windows.Controls;
+using ClientDesktop.Views;
+using Core.Common;
+using Core.Common.Contracts;
+using Core.Common.UI.Core;
+using Core.Common.Utils;
+using CSC3045.Agile.Client.Contracts;
+using CSC3045.Agile.Client.CustomPrinciples;
+using CSC3045.Agile.Client.Entities;
+using Prism.Regions;
 
 namespace ClientDesktop.ViewModels
 {
-    class ManageSprintViewModel
+    [Export]
+    [PartCreationPolicy(CreationPolicy.NonShared)]
+    public class ManageSprintViewModel : ViewModelBase
     {
     }
 }
